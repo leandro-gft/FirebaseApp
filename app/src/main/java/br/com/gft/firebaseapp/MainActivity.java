380962beb8a791927b243aa9e6f7778590364034
leandro-gft <lpsacchi@gmail.com>
2020-06-04 11:44:58 -0300

    Adicionada dependencia do Realtime Database

--- app/src/main/java/br/com/gft/firebaseapp/MainActivity.java
+++ app/src/main/java/br/com/gft/firebaseapp/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

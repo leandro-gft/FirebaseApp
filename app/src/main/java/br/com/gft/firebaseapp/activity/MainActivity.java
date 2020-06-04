ae325ff01244fd7e92639f4be3fff393c3f6b778
leandro-gft <lpsacchi@gmail.com>
2020-06-04 12:44:42 -0300

    Recuperando dados do Firebase

--- app/src/main/java/br/com/gft/firebaseapp/activity/MainActivity.java
+++ app/src/main/java/br/com/gft/firebaseapp/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package br.com.gft.firebaseapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.gft.firebaseapp.R;
 import br.com.gft.firebaseapp.model.Produto;
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Salvando e atualizando dados
 //        referencia.child("usuarios").child("003").child("nome").setValue("Claudia");
 //        referencia.child("usuarios").child("003").child("sobrenome").setValue("Sacchi");
 
@@ -32,8 +38,25 @@ public class MainActivity extends AppCompatActivity {
 //        Usuario usuario = new Usuario("Jamilton","Damasceno",30);
 //        usuarios.child("004").setValue(usuario);
 
-        DatabaseReference produtos = referencia.child("produtos");
-        Produto produto = new Produto("Notebook i7 1TB 8GB", "Asus", 2500.00);
-        produtos.child("003").setValue(produto);
+//        DatabaseReference produtos = referencia.child("produtos");
+//        Produto produto = new Produto("Notebook i7 1TB 8GB", "Asus", 2500.00);
+//        produtos.child("003").setValue(produto);
+
+        //Recuperando dados
+        DatabaseReference usuarios = referencia.child("usuarios").child("001");
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE", dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 }
